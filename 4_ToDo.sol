1da7801a1da02d80e598d554bebb019ca625f29e
Tasmai <130929696+Tasmai@users.noreply.github.com>
2023-10-05 04:08:27 -0700

    Add files via upload

---
+++ 4_ToDo.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL 3.0
+pragma solidity > 0.4.0 <= 0.9.0;
+
+contract ToDo 
+{
+    struct task
+    {
+        bool iscomplete;
+        string description;
+    }
+
+    mapping(address => task[]) private users;
+
+    function addTask(string calldata _description) external 
+    {
+        users[msg.sender].push(task
+        (
+            {
+            iscomplete : false, 
+            description : _description
+            }
+        )
+        );
+    } 
+
+    function getCount() external view returns (uint)
+    {
+        return [msg.sender].length;
+    } 
+
+    function deleteTask(uint id) external 
+    {
+        delete users[msg.sender][id];
+    } 
+}
